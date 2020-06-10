8e269f910428a80c8bc510b456f9c5ec01743339
MarcosVinicios1990 <mvinicios1990@gmail.com>
2020-06-09 22:49:28 -0300

    inicio php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "Olá Mundo!";
+
+echo date('d/m/y');
+
+$nome = "Marcos Vinicios";
+$idade = 30;
+
+echo $nome." tem a idade de: ".$idade;
+
+$nome = "João Pedro";
+$idade = 28.5;
+
+echo $nome." tem a idade de: ".$idade;  
+
+var_dump($idade);
+
+$numero1 = 250;
+$numero2 = 3000;
+
+$resultado = $numero1 + $numero2;
+
+echo $resultado;
+
+?>
